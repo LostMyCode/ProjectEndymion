894948faf2f58e5f55289f25e90691996f9b1695
LostMyCode <studionyna@gmail.com>
2020-07-06 17:48:32 +0900

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,51 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // const mode = 'production'
-const mode = 'development'
-const masterURL = mode == 'production' ? 'https://ex-script.com/fstyle/hslo/' : 'http://127.0.0.1:5500/dist/'
 
-module.exports = {
-    devtool: mode == "development" ? "#source-map" : "",
-    entry: ['./src/index.js'],
-    mode,
-    output: {
-        path: __dirname + '/dist',
-        filename: 'Endymion.pack.[hash].js',
-        publicPath: mode == "production" ? masterURL + "Endymion/" : masterURL
-    },
+module.exports = (env, options) => {
+    const mode = options.mode || 'development'
+    const masterURL = mode == 'production' ? 'https://ex-script.com/fstyle/hslo/' : 'http://127.0.0.1:5500/dist/'
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',   //loader名
-                    options: {                //Babelの設定
-                        presets: ['@babel/preset-env']
+    const config =  {
+        devtool: mode == "development" ? "#source-map" : "",
+        entry: ['./src/index.js'],
+        mode,
+        output: {
+            path: __dirname + '/dist',
+            filename: 'Endymion.pack.[hash].js',
+            publicPath: mode == "production" ? masterURL + "Endymion/" : masterURL
+        },
+    
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',   //loader名
+                        options: {                //Babelの設定
+                            presets: ['@babel/preset-env']
+                        }
                     }
+                },
+                {
+                    test: /\.html$/,
+                    loader: "html-loader"
                 }
-            },
-            {
-                test: /\.html$/,
-                loader: "html-loader"
-            }
-        ]
-    },
+            ]
+        },
+    
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: mode == "production" ? "./src/html/index.html" : "./src/html/debug.html",
+                filename: mode == "production" ? "index.php" : "index.html",
+            })
+        ],
+    
+        resolve: {
+            extensions: [".js"]
+        }
+    };
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: mode == "production" ? "./src/html/index.html" : "./src/html/debug.html",
-            filename: mode == "production" ? "index.php" : "index.html",
-        })
-    ],
-
-    resolve: {
-        extensions: [".js"]
-    }
-};
+    return config;
+}
