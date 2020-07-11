233bd3bf7b5723c905b89909ccb28e7f4eec48fb
LostMyCode <studionyna@gmail.com>
2020-07-11 20:51:34 +0900

    Added github pages url

--- src/Before.js
+++ src/Before.js
@@ -1,6 +1,7 @@
 console.info("[test114514] HSLO Endymion v4.1a");
 
 window.masterURL = "https://ex-script.com/fstyle/hslo/";
+window.GithubPagesURL = "https://lostmycode.github.io/ProjectEndymion/";
 
 window.WebSocketDayo = WebSocket;
 window.WebSocketDayo._send = window.WebSocketDayo.send;
